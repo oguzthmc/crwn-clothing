ba1c3e5d0651aa45f9312fc7da1629b7099e2e46
Oğuzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2020-06-26 00:44:29 +0300

    using redux-thunk for asynchronous event handling, switching collectionsOverview and collectionsPage to use containers HOC, updating code to leverage containers for collections-overview and collection-page components

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -9,13 +9,23 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
-    collections => 
+    collections =>
         collections ? Object.keys(collections).map(key => collections[key]) : []
 );
 
 // find collection.id matching the url parameter of our collection id map
-export const selectCollection = collectionUrlParam => 
-createSelector(
-    [selectCollections],
-    collections => (collections ? collections[collectionUrlParam] : null)
+export const selectCollection = collectionUrlParam =>
+    createSelector(
+        [selectCollections],
+        collections => (collections ? collections[collectionUrlParam] : null)
+    );
+
+export const selectIsCollectionFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+    [selectShop],
+    shop => !!shop.collections
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';      //this is used when debugging our redux code
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
