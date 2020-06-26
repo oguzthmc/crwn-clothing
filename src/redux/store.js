b2560be9dcccc2ca5b4f87a8169f3726ff9efc9b
Oğuzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2020-06-26 23:23:57 +0300

    switching collectionsOverview and collectionsPage to use containers HOC

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,9 +11,8 @@ if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
 }
 
-
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
 
-export default { store, persistor };
+export default { store, persistStore };
