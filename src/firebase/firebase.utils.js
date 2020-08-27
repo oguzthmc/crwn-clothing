1c7cd52fc9a032110aabe64d43b44ce670a1a843
Oğuzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2020-08-27 14:53:18 +0300

    created user and cart sagas for sign in, sign up and sign in with google

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -70,11 +70,22 @@ export const convertCollectionsSnapshotToMap = (collections) => {
     }, {});
 };
 
+// Remember: We're just "mimicking" functionality that 
+// you may encounter when you don't have Firebase as the backend.
+export const getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+        const unsubscribe = auth.onAuthStateChanged(userAuth => {
+            unsubscribe();
+            resolve(userAuth);
+        }, reject)
+    });
+}
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 export default firebase;
